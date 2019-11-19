bef16f6c7333b93c962b545f44e97371c68992e4
reedas <reedas@users.noreply.github.com>
2019-11-19 10:04:48 +0000

    Basic Hello World tft display library program

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
+/* Hello World! for the TextLCD Enhanced Library*/
+
 #include "mbed.h"
-#include "TextLCD.h"
+#include "GUI.h"
+#include "cy8ckit_028_tft.h"
 
-TextLCD lcd(p9, p10);
+void Display_Init(void)
+{
 
-int main() {
-    lcd.printf("Hi mbed World!\n");
+    /* Set font size, foreground and background Colours */
+    GUI_SetFont(GUI_FONT_16B_1);
+    GUI_SetColor(GUI_WHITE);
+    GUI_SetBkColor(GUI_BLACK);
+
+    /* Clear screen and print splash screen */
+    GUI_Clear();
+    GUI_SetTextAlign(GUI_TA_HCENTER);
+    GUI_DispStringAt("TFT Demo", 160, 20);
 }
+
+int main()
+{
+    /* Initialise EmWin driver*/
+    GUI_Init();
+
+    /* Initialise display */
+    Display_Init();
+
+
+    GUI_SetFont(GUI_FONT_8X16X2X2);
+        GUI_SetTextAlign(GUI_TA_HCENTER);
+        GUI_DispStringAt("Hello World!", 160, 200);
+
+}
+
+
+
